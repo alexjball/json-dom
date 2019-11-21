53c3d3474d1f4151d192c71f84cd8487f39c9077
Alex Ball <ball.alex.509@gmail.com>
2019-11-20 20:31:58 -0500

    Add cli.js and usage instructions

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Parse and query a view hierarchy specified in json
 
 `SystemViewController.json` is copied from https://github.com/jdolan/quetoo/blob/master/src/cgame/default/ui/settings/SystemViewController.json
+
+# Usage
+
+```
+git clone https://github.com/alexjball/json-dom.git
+cd json-dom
+npm install
+./cli.js
+```
---
+++ cli.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const readline = require("readline");
+const yargs = require("yargs");
+const fs = require("fs");
+const JsonDom = require("./JsonDom");
+
+const argv = yargs
+  .option("file", {
+    description: "JSON file containing the view hierarchy to query"
+  })
+  .demandOption("file", "Please specify the view file to query").argv;
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const dom = JsonDom.parse(fs.readFileSync(argv.file, { encoding: "utf8" }));
+const repl = () => {
+  terminal.question("Selector to query: ", rule => {
+    try {
+      const matchedViews = dom.matchSelector(rule);
+      console.log(`Matched ${matchedViews.length} views`);
+      console.log(matchedViews);
+    } catch (e) {
+      console.error("Couldn't match selector", e);
+    }
+    repl();
+  });
+};
+
+repl();
